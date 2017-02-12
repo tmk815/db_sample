b32c02563bddc6edfd54e5d2ddc86859e4f75587
門田朋己 <monndenn15@gmail.com>
2017-02-12 22:08:42 +0900

    add getter and setter

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -10,5 +10,53 @@ public class SampleController {
 
     public SampleController(String v1,String v2,String v3,String v4,String v5,String v6){
         this.v1=v1;
+        this.v2=v2;
+        this.v3=v3;
+        this.v4=v4;
+        this.v5=v5;
+        this.v6=v6;
+    }
+
+    public void setValue1(String v){
+        v1=v;
+    }
+
+    public String getValue1(){
+        return v1;
+    }
+    public void setValue2(String v){
+        v2=v;
+    }
+
+    public String getValue2(){
+        return v2;
+    }
+    public void setValue3(String v){
+        v3=v;
+    }
+
+    public String getValue3(){
+        return v3;
+    }
+    public void setValue4(String v){
+        v4=v;
+    }
+
+    public String getValue4(){
+        return v4;
+    }
+    public void setValue5(String v){
+        v5=v;
+    }
+
+    public String getValue5(){
+        return v5;
+    }
+    public void setValue6(String v){
+        v6=v;
+    }
+
+    public String getValue6(){
+        return v6;
     }
 }

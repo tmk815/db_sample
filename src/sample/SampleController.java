9c8d8ec34fdc7a025dd9aebf5fc2d83c991cf1b5
門田朋己 <monndenn15@gmail.com>
2017-02-12 21:53:08 +0900

    create db_test

---
+++ src/sample/SampleController.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class SampleController {
+    protected String v1;
+    protected String v2;
+    protected String v3;
+    protected String v4;
+    protected String v5;
+    protected String v6;
+
+    public SampleController(String v1,String v2,String v3,String v4,String v5,String v6){
+        this.v1=v1;
+    }
+}

373c32b2c22ee82ea1b5aa2e1c401616104c989a
門田朋己 <monndenn15@gmail.com>
2017-03-17 22:10:28 +0900

    change import

--- src/sample/TableController.java
+++ src/sample/TableController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
